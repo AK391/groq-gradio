6866a2a5ca91a842e4f73854cc0ae2a35b33350c
AK391 <ahsen@huggingface.co>
2024-10-11 19:24:54 -0400

    update

--- custom_app.py
+++ custom_app.py
@@ -2,9 +2,9 @@ import gradio as gr
 import groq_gradio
 
 gr.load(
-    name='gpt-4-turbo',
-    src=openai_gradio.registry,
-    title='OpenAI-Gradio Integration',
-    description="Chat with gpt-4-turbo model.",
+    name='llama-3.1-70b-versatile',
+    src=groq_gradio.registry,
+    title='Groq-Gradio Integration',
+    description="Chat with llama-3.1-70b-versatile model.",
     examples=["Explain quantum gravity to a 5-year old.", "How many R are there in the word Strawberry?"]
 ).launch()
